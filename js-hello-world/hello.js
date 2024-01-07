76c14679431c82f4b06ee80284dde0d4b2281124
Billie-LS <lskylizard@yahoo.com>
2024-01-07 11:48:55 -0600

    JS hello world exercises

--- js-hello-world/hello.js
+++ js-hello-world/hello.js
@@ -15,14 +15,41 @@ console.log('Quoth the Raven "Nevermore"', " Hello, World!");
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
-
-   1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
-   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
-   4. Remove the semi-colon, ;.
-   5. Print a number. (Bonus: Print two numbers added together).
-   6. Print multiple messages one after the other.
-   7. Print two messages on the same line.
-   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
-   9. Other. You choose!
 */
+
+//    1. Change the message that is printed.
+console.log("Goodbye, Folks!");
+
+//  2. Figure out what the parentheses do. Will the code work without them? NO
+console.log ("Hello, World!");
+//  3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
+console.log ("Hello, World!");
+console.log ('Hello, World!');
+//    4. Remove the semi-colon, ;.
+console.log("Hello, World!")
+console.log ('Hello, World!')
+//    5. Print a number. (Bonus: Print two numbers added together).
+console.log(2);
+// addition
+console.log(2+2);
+// multiplication
+console.log(2*2);
+// division
+console.log(2/2);
+// modulus
+console.log(2%2);
+// exponentiation
+console.log(2**2);
+
+//    6. Print multiple messages one after the other.
+console.log('testing single quotes');
+console.log('it worked!');
+
+//    7. Print two messages on the same line.
+console.log("testing message 1", "testing message 2");
+
+//    8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+console.log('Quoth the Raven "Nevermore"');
+
+//    9. Other. You choose!
+console.log('Quoth the Raven "Nevermore"', " Hello, World!");
