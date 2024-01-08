e0259768ae1c3031c78550827f784481510bef7d
Billie-LS <lskylizard@yahoo.com>
2024-01-08 13:58:50 -0600

    variables

---
+++ js-hello-world/variables.js
@@ -0,0 +1,23 @@
+/*
+variable is a name that refers to a value
+a value is a single, specific piece of data, 
+such as a specific number or string. 
+Variables allow us to store values for later use
+*/
+
+// Declaring and Initializing Variables With keyword let
+
+// Declaring variable 'programmingLanguage' with keyword let
+let programmingLanguage; // variable declaration
+
+/*
+a declared variable can then be given a value using an assignment statement,
+which uses = to give a variable a value
+*/
+
+// The act of assigning a variable a value for the first time is called initialization
+let ourProgrammingLanguage;  // variable declaration
+ourProgrammingLanguage = "JavaScript";  // initialization
+
+// declare and initialize a variable with a single line of code
+let theProgrammingLanguage = 'javascript'; // declaration AND initialization
